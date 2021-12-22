868bd0cee30b68afb9dbb7412ec7f1cbbcda715f
bellabula <zz225864@gmail.com>
2021-12-22 17:54:39 +0800

    write to csv

--- products.py
+++ products.py
@@ -4,8 +4,13 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
+    price = int(price)
     products.append([name, price])
 print(products)
 
 for p in products:
     print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
